35030a9422f1c9428b1c9302f76f282543ba6ae0
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-04-12 22:35:12 +0900

    Create BoardVO.java

---
+++ code/java/src/vo/BoardVO.java
@@ -0,0 +1,46 @@
+package vo;
+
+public class BoardVO {
+	String title, content, author, date;
+	int view;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public int getView() {
+		return view;
+	}
+
+	public void setView(int view) {
+		this.view = view;
+	}
+}
