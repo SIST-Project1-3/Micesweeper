9e6d38718e091ea2c76325d611b08b5244750d5b
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-04-12 16:44:07 +0900

    Create BoardListUI.java

---
+++ code/java/src/gui/BoardListUI.java
@@ -0,0 +1,24 @@
+package gui;
+
+import javax.swing.JFrame;
+
+public class BoardListUI {
+
+	// Field
+	private JFrame frame;
+
+	// Constructor
+	public BoardListUI() {
+		initialize();
+	}
+
+	// Method
+	private void initialize() {
+		frame = new JFrame();
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new BoardListUI();
+	}
+}
