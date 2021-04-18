6330cc1b5fd04a503763ff9d9752d8402c557129
js950830 <82020559+js950830@users.noreply.github.com>
2021-04-18 22:54:36 +0900

    Create GameSystemClient.java

---
+++ code/java/src/system/GameSystemClient.java
@@ -0,0 +1,15 @@
+package system;
+
+public class GameSystemClient {
+//field
+	
+//constructor
+	public GameSystemClient() {
+		init();
+	}
+//method
+	public void init() {
+		//서버로부터 GameVO 받아옴
+		
+	}
+}
