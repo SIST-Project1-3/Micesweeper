6f9e13cf902308f5fb7485ec25c8819612e1cc29
js950830 <82020559+js950830@users.noreply.github.com>
2021-04-18 22:54:30 +0900

    Update GameVO.java

--- code/java/src/vo/GameVO.java
+++ code/java/src/vo/GameVO.java
@@ -1,6 +1,18 @@
 package vo;
 
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+
 // 게임 내 정보들을 담기위한 VO
 public class GameVO {
-	int[] mice = new int[81];
+	ArrayList<Integer> mice;
+
+	public ArrayList<Integer> getMice() {
+		return mice;
+	}
+
+	public void setMice(ArrayList<Integer> mice) {
+		this.mice = mice;
+	}
 }
