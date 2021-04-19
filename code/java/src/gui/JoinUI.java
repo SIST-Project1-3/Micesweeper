c1e89a0d856da03af7c97f145f73894edfb35322
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-04-20 01:19:04 +0900

    Update JoinUI.java
    실행을 위한 main 메소드 파라미터 설정

--- code/java/src/gui/JoinUI.java
+++ code/java/src/gui/JoinUI.java
@@ -14,18 +14,22 @@ import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
+import system.client.ClientSystem;
+
 public class JoinUI {
-	JoinUIEvent joinEvent = new JoinUIEvent(this);
 	JFrame f;
-	JPanel west_panel, center_panel, south_panel,
-		  label_panel, tf_panel, btn_panel, id_check_panel;
+	JPanel west_panel, center_panel, south_panel, label_panel, tf_panel, btn_panel, id_check_panel;
 	JButton join_btn, id_check_btn, cancel_btn;
 	JTextField id_tf;
 	JPasswordField pw_tf, pw_check_tf;
-	String[] joinlist = {"ID","PW","PW확인"};
+	String[] joinlist = { "ID", "PW", "PW확인" };
 	ArrayList<Object> list = new ArrayList<Object>();
-	
-	public JoinUI() {
+	ClientSystem client;
+	JoinUIEvent joinEvent;
+
+	public JoinUI(ClientSystem client) {
+		this.client = client;
+		joinEvent = new JoinUIEvent(this, client);
 		init();
 	}
 
@@ -38,28 +42,28 @@ public class JoinUI {
 		tf_panel = new JPanel(new GridLayout(4, 1));
 		btn_panel = new JPanel(new FlowLayout());
 		id_check_panel = new JPanel(new FlowLayout());
-		
-		//하단의 가입, 취소 버튼 생성
+
+		// 하단의 가입, 취소 버튼 생성
 		join_btn = new JButton("가입");
 		cancel_btn = new JButton("취소");
 		join_btn.setFont(Commons.getFont());
 		cancel_btn.setFont(Commons.getFont());
 		btn_panel.add(join_btn);
 		btn_panel.add(cancel_btn);
-		
-		//회원가입 정보 입력창 생성을 위한 for문
-		//텍스트필드에 포커스 맞출까..?
-		for(String join : joinlist) {
+
+		// 회원가입 정보 입력창 생성을 위한 for문
+		// 텍스트필드에 포커스 맞출까..?
+		for (String join : joinlist) {
 			JLabel join_label = new JLabel(join);
 			join_label.setFont(Commons.getFont());
-			JPanel l_panel = new JPanel(new FlowLayout(FlowLayout.LEFT,0,7));
+			JPanel l_panel = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 7));
 			JPanel t_panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-	
-			//중복확인 버튼을 ID텍스트필드 옆에 생성하기 위한 if문
-			if(join.equals("ID")) {
+
+			// 중복확인 버튼을 ID텍스트필드 옆에 생성하기 위한 if문
+			if (join.equals("ID")) {
 				l_panel.add(join_label);
 				label_panel.add(l_panel);
-				
+
 				id_tf = new JTextField(15);
 				id_check_btn = new JButton("중복확인");
 				id_check_btn.setFont(Commons.getFont());
@@ -67,14 +71,14 @@ public class JoinUI {
 				id_check_panel.add(id_check_btn);
 				tf_panel.add(id_check_panel);
 				list.add(id_tf);
-			}else if(join.equals("PW")){
+			} else if (join.equals("PW")) {
 				pw_tf = new JPasswordField(23);
 				l_panel.add(join_label);
 				t_panel.add(pw_tf);
 				label_panel.add(l_panel);
 				tf_panel.add(t_panel);
 				list.add(pw_tf);
-			}else if(join.equals("PW확인")) {
+			} else if (join.equals("PW확인")) {
 				pw_check_tf = new JPasswordField(23);
 				l_panel.add(join_label);
 				t_panel.add(pw_check_tf);
@@ -83,34 +87,33 @@ public class JoinUI {
 				list.add(pw_check_tf);
 			}
 		}
-		
+
 		join_btn.addActionListener(joinEvent);
 		id_check_btn.addActionListener(joinEvent);
 		cancel_btn.addActionListener(joinEvent);
-		
+
 		west_panel.add(label_panel);
 		center_panel.add(tf_panel);
 		south_panel.add(btn_panel);
-		
+
 		f.add(BorderLayout.WEST, west_panel);
 		f.add(BorderLayout.CENTER, center_panel);
 		f.add(BorderLayout.SOUTH, south_panel);
-		
-		//회원가입창 종료
+
+		// 회원가입창 종료
 		f.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				f.dispose();
 			}
 		});
-		
-		f.setResizable(false);	//창크기 변경 막아놓음
+
+		f.setResizable(false);	// 창크기 변경 막아놓음
 		f.setSize(400, 200);
 		f.setVisible(true);
 	}
-	
+
 	public static void main(String[] args) {
-		new JoinUI();
+		new JoinUI(new ClientSystem());
 	}
-	
-	
+
 }
