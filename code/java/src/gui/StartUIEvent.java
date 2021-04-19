ca3426a4c97ed48e01cccadd0798e15347014580
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-04-20 01:21:26 +0900

    Update StartUIEvent.java
    - 클라이언트에서 처리해야할 부분을 시스템에서 처리하여 클라이언트를 호출하도록 변경.
    - 로그인 버튼뿐만 아니라 기타 textfield에서 엔터를 실행해도 로그인 메소드가 실행되도록 추가.
    - main 메소드 삭제

--- code/java/src/gui/StartUIEvent.java
+++ code/java/src/gui/StartUIEvent.java
@@ -5,58 +5,50 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JOptionPane;
 
-import system.server.ServerSystem;
+import system.client.ClientSystem;
 
-public class StartUIEvent implements ActionListener{
+public class StartUIEvent implements ActionListener {
 	StartUI sui;
-	ServerSystem system;
-	
-	public StartUIEvent(StartUI sui) {
+	ClientSystem client;
+
+	public StartUIEvent(StartUI sui, ClientSystem client) {
 		this.sui = sui;
+		this.client = client;
 	}
-	
-	public StartUIEvent(StartUI sui, ServerSystem system) {
-		this.sui = sui;
-		this.system = system;
-	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		Object obj = e.getSource();
-		if(obj == sui.login_btn) {
+		if (obj == sui.login_btn || obj == sui.id_tf || obj == sui.pw_tf) {
 			login_check();
-		}else if(obj == sui.join_btn) {
-			new JoinUI();
+		} else if (obj == sui.join_btn) {
+			new JoinUI(client);
 		}
-	}//액션 이벤트
-	
-	//로그인 유효성 검사
+	}// 액션 이벤트
+
+	// 로그인 유효성 검사
 	public void login_check() {
-		if(sui.id_tf.getText().equals("")) {
+		if (sui.id_tf.getText().equals("")) {
 			JOptionPane.showMessageDialog(null, Commons.getMsg("아이디를 입력해주세요"));
 			sui.id_tf.requestFocus();
-		}else if(sui.pw_tf.getText().equals("")) {
+		} else if (sui.pw_tf.getText().equals("")) {
 			JOptionPane.showMessageDialog(null, Commons.getMsg("패스워드를 입력해주세요"));
 			sui.pw_tf.requestFocus();
-		}else {
-			//로그인 체크
-			boolean result = system.loginCheck(sui.id_tf.getText(), sui.pw_tf.getText()); 
-			if(result) {
+		} else {
+			// 로그인 체크
+			boolean result = client.loginCheck(sui.id_tf.getText(), sui.pw_tf.getText());
+			if (result) {
 				JOptionPane.showMessageDialog(null, Commons.getMsg("로그인 되었습니다."));
-				new MainUI(sui.client);
+				client.setId(sui.id_tf.getText());
+				new MainUI(client);
 				sui.f.setVisible(false);
-			}else {
+			} else {
 				JOptionPane.showMessageDialog(null, Commons.getMsg("아이디 또는 패스워드가 틀렸습니다."));
 				sui.id_tf.setText("");
 				sui.pw_tf.setText("");
 				sui.id_tf.requestFocus();
 			}
 		}
-	}//login_check
-	
-	public static void main(String[] args) {
-		new StartUI();
-	}
-	
-	
+	}// login_check
+
 }
