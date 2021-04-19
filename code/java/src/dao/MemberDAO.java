fee29ea25bf10ceff7802e7a21a427c4e7a744a1
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-04-20 01:18:02 +0900

    Update MemberDAO.java
    - 콘솔에 어떤 DAO가 생성됐는지 확인을 위해 print()문 추가
    - getJoinResult(): 쿼리구문 Insert 구문의 입력 파라미터 개수 설정을 통해 에러 제거
    - getIdCheckResult(): rs.next()가 없어 실행이 안되는 메소드이므로 해당 코드 추가

--- code/java/src/dao/MemberDAO.java
+++ code/java/src/dao/MemberDAO.java
@@ -3,68 +3,75 @@ package dao;
 import vo.MemberVO;
 
 public class MemberDAO extends DAO {
+	// Constructor
+	public MemberDAO() {
+		System.out.println("3: MemberDAO load");
+	}
 
+	// Method
 	// 회원가입
 	public boolean getJoinResult(MemberVO member) {
 		boolean result = false;
-		
+
 		try {
-			String sql = "insert into member values(?,?)";
+			String sql = "insert into member(ID, PW) values(?,?)";
 			getPreparedStatement(sql);
-			
+
 			pstmt.setString(1, member.getId());
 			pstmt.setString(2, member.getPw());
-			
+
 			int val = pstmt.executeUpdate();
-			if(val != 0) result = true;
-			
+			if (val != 0)
+				result = true;
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return result;
 	}
-	
+
 	// 아이디 중복확인
 	public boolean getIdCheckResult(String id) {
 		boolean result = false;
-		
+
 		try {
 			String sql = "select count(*) from member where id=?";
 			getPreparedStatement(sql);
-			
+
 			pstmt.setString(1, id);
-			
+
 			rs = pstmt.executeQuery();
-			if(rs.getInt(1) == 1) result = true;
-			
+			while (rs.next()) {
+				if (rs.getInt(1) == 1)
+					result = true;
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return result;
 	}
-	
+
 	// 로그인
 	public boolean getLoginResult(String id, String pw) {
 		boolean result = false;
-		
+
 		try {
 			String sql = "select count(*) from member where id=? and pw=?";
 			getPreparedStatement(sql);
-			
+
 			pstmt.setString(1, id);
 			pstmt.setString(2, pw);
-			
+
 			rs = pstmt.executeQuery();
-			while(rs.next()) {
-				if(rs.getInt(1) == 1) result = true;
+			while (rs.next()) {
+				if (rs.getInt(1) == 1)
+					result = true;
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return result;
 	}
-	
-	
-	
+
 }
