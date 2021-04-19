e2ec3447d0d1e8dfad05d79756a005f8df968561
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-04-19 14:56:52 +0900

    Update BoardWriteUI.java
    프레임 종료 기능 추가

--- code/java/src/gui/BoardWriteUI.java
+++ code/java/src/gui/BoardWriteUI.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -62,6 +64,17 @@ public class BoardWriteUI implements ActionListener {
 		frame.add(panel_btn, "South");
 		frame.setSize(500, 500);
 		frame.setVisible(true);
+
+		frame.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				exit();
+			}
+		});
+	}
+
+	public void exit() {
+		frame.dispose();
 	}
 
 	@Override
@@ -80,6 +93,7 @@ public class BoardWriteUI implements ActionListener {
 				msg.setContent(tf_content.getText());
 				if (client.writeBoard(msg)) {
 					JOptionPane.showMessageDialog(null, Commons.getMsg("글 작성 성공"));
+					frame.dispose();
 				} else {
 					JOptionPane.showMessageDialog(null, Commons.getMsg("글 작성 실패"));
 				}
