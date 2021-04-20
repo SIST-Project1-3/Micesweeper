e2383ef8d915e8f43ba5ee31b2b9ef06c6df2097
ohdavi <74041787+ohdavi@users.noreply.github.com>
2021-04-20 22:01:58 +0900

    Update JoinUIEvent.java
    JoinUIEvent -  pw확인 빈칸일때 가입버튼 누르면 pw확인을 입력해주세요 가 아닌 비밀번호가 다릅니다 메세지가 뜨는 부분 수정 완료, 아이디 중복확인 오류 수정중

--- code/java/src/gui/JoinUIEvent.java
+++ code/java/src/gui/JoinUIEvent.java
@@ -24,23 +24,27 @@ public class JoinUIEvent implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		Object obj = e.getSource();
+		// 아이디 중복확인 버튼
 		if (obj == jui.id_check_btn) {
+			// 아이디 입력창 빈칸 검사
 			if (jui.id_tf.getText().equals("")) {
 				JOptionPane.showMessageDialog(null, Commons.getMsg("아이디를 입력해주세요."));
 				jui.id_tf.requestFocus();
 			} else if (!jui.list.get(0).equals("")) {
+				// 아이디 입력창 빈칸 아닐 때 중복검사
 				id_check();
 			}
+			// 가입 버튼
 		} else if (obj == jui.join_btn) {
-			// 아이디 중복확인 안한 경우 가입 못하게 하기
-			//boolean result = id_check();
-			boolean result = client.idCheck(jui.id_tf.getText());
-			
+			// 아이디 중복확인 안한 경우(result=false) 가입 안되게 하기
+//			boolean result = client.idCheck(jui.id_tf.getText());
+			boolean result = id_check();
+
 			if (!result) {
-				//PW와 PW확인이 같은지 확인
-				if(jui.pw_tf.getText().equals(jui.pw_check_tf.getText())) {
-					//회원가입 정보 저장 및 회원가입 가능 확인
+				// PW == PW확인 이 맞는지 체크
+				if (jui.pw_tf.getText().equals(jui.pw_check_tf.getText())) {
 					if (join_check()) {
+						// 회원가입 정보 저장 및 회원가입 성공여부 확인
 						ArrayList<JTextField> jlist = new ArrayList<JTextField>();
 						for (Object tf : jui.list) {
 							JTextField jtf = (JTextField) tf;
@@ -49,7 +53,7 @@ public class JoinUIEvent implements ActionListener {
 						MemberVO member = new MemberVO();
 						member.setId(jlist.get(0).getText());
 						member.setPw(jlist.get(1).getText());
-						
+
 						boolean result2 = client.joinCheck(member);
 						if (result2) {
 							JOptionPane.showMessageDialog(null, Commons.getMsg("회원가입을 완료했습니다."));
@@ -58,7 +62,6 @@ public class JoinUIEvent implements ActionListener {
 								tf.setText("");
 							}
 							jui.f.setVisible(false);
-							
 						} else {
 							JOptionPane.showMessageDialog(null, Commons.getMsg("회원가입에 실패했습니다."));
 						}
@@ -66,18 +69,21 @@ public class JoinUIEvent implements ActionListener {
 						for (Object obj2 : jui.list) {
 							JTextField tf = (JTextField) obj2;
 						}
-					}// if문(join_check)
-				} else if(jui.pw_check_tf.getText().equals("")){
+					} // if문(join_check)
+					// PW확인이 비어있을 때
+				} else if (jui.pw_check_tf.getText().isEmpty()) {
 					JOptionPane.showMessageDialog(null, Commons.getMsg("PW확인을 입력해주세요."));
 					jui.pw_check_tf.requestFocus();
-				}else {
+					// PW != PW확인
+				} else {
 					JOptionPane.showMessageDialog(null, Commons.getMsg("비밀번호가 다릅니다."));
 					jui.pw_tf.requestFocus();
 				}
 			} else {
 				JOptionPane.showMessageDialog(null, Commons.getMsg("아이디 중복여부를 확인해주세요."));
 				jui.id_tf.requestFocus();
-			}// if문(id_check)
+			}
+			// 취소 버튼
 		} else if (obj == jui.cancel_btn) {
 			// 회원가입창 종료
 			jui.f.dispose();
@@ -86,19 +92,18 @@ public class JoinUIEvent implements ActionListener {
 
 	// ID 중복 체크
 	public boolean id_check() {
-		//boolean result = false;
-		// result2가 true면 중복된 아이디가 있다는 의미
-		boolean result2 = client.idCheck(jui.id_tf.getText());
-		
-		if (result2) {
+		boolean result = true;
+		// result가 true면 중복된 아이디가 있다는 의미
+
+		if (result == client.idCheck(jui.id_tf.getText())) {
 			JOptionPane.showMessageDialog(null, Commons.getMsg("이미 사용중인 아이디입니다"));
 			jui.id_tf.requestFocus();
 		} else {
 			JOptionPane.showMessageDialog(null, Commons.getMsg("사용가능한 아이디입니다"));
 			jui.pw_tf.requestFocus();
-			result2 = true; 
+			result = false;
 		}
-		return result2;
+		return result;
 	}// id_check
 
 	// 회원가입 빈칸 체크
@@ -108,7 +113,6 @@ public class JoinUIEvent implements ActionListener {
 		for (int i = 0; i < jui.list.size(); i++) {
 			JTextField tf = (JTextField) jui.list.get(i);
 
-			// 빈칸 검사
 			if (tf.getText().equals("")) {
 				JOptionPane.showMessageDialog(null, Commons.getMsg(jui.joinlist[i] + "를 입력해주세요"));
 				tf.requestFocus();
@@ -119,8 +123,6 @@ public class JoinUIEvent implements ActionListener {
 			}
 		}
 		return result;
-		
-		
 	}// join_check
 
 }
