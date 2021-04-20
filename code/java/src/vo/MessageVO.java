bc9f90e95c5991c8abf6bfcde220e9b75498f877
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-04-20 12:46:17 +0900

    Update MessageVO.java
    - 필드 주석 추가

--- code/java/src/vo/MessageVO.java
+++ code/java/src/vo/MessageVO.java
@@ -5,12 +5,12 @@ import java.util.ArrayList;
 
 // 서버와 클라이언트가 통신할 때 사용하는 VO
 public class MessageVO implements Serializable {
-	public static int BOARD_WRITE = 1;
-	public static int BOARD_READ_LIST = 2;
-	public static int BOARD_READ_ARTICLE = 3;
-	int status, no;
-	String title, content, id;
-	boolean result;
+	public static int BOARD_WRITE = 1; // 게시글 작성
+	public static int BOARD_READ_LIST = 2; // 게시글 목록 불러오기 
+	public static int BOARD_READ_ARTICLE = 3; // 특정 게시글 읽기 
+	int status, no; // MsgVO의 상태, 게시글 No
+	String title, content, id; // 게시글 제목, 내용, 작성자
+	boolean result; // DAO 결과값
 	ArrayList<BoardVO> boardList;
 
 	public int getStatus() {
