e6f3410f52571b92e35bcc142d473279ee917e82
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-04-20 15:57:58 +0900

    Update MessageVO.java
    - 주석 정리
    - 게시글 읽기 기능을 위한 article 필드 추가

--- code/java/src/vo/MessageVO.java
+++ code/java/src/vo/MessageVO.java
@@ -6,12 +6,17 @@ import java.util.ArrayList;
 // 서버와 클라이언트가 통신할 때 사용하는 VO
 public class MessageVO implements Serializable {
 	public static int BOARD_WRITE = 1; // 게시글 작성
-	public static int BOARD_READ_LIST = 2; // 게시글 목록 불러오기 
-	public static int BOARD_READ_ARTICLE = 3; // 특정 게시글 읽기 
+	public static int BOARD_READ_LIST = 2; // 게시글 목록 불러오기
+	public static int BOARD_READ_ARTICLE = 3; // 특정 게시글 읽기
+	public static int LOGIN = 4; // 로그인
+	public static int LOGOUT = 5; // 게임종료 및 로그아웃
+	public static int JOIN = 6; // 회원가입
+	public static int SERVERCHAT = 7;
 	int status, no; // MsgVO의 상태, 게시글 No
-	String title, content, id; // 게시글 제목, 내용, 작성자
+	String title, content, id; // 게시글 제목, (내용, 채팅메시지), 작성자
 	boolean result; // DAO 결과값
-	ArrayList<BoardVO> boardList;
+	ArrayList<BoardVO> boardList; // 게시글 목록 불러오기
+	BoardVO article; // 게시글 읽기
 
 	public int getStatus() {
 		return status;
@@ -67,6 +72,14 @@ public class MessageVO implements Serializable {
 
 	public void setNo(int no) {
 		this.no = no;
+	}
+
+	public BoardVO getArticle() {
+		return article;
+	}
+
+	public void setArticle(BoardVO article) {
+		this.article = article;
 	}
 
 }
