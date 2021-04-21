d0a1ce80991bdcdf0baa22c341068720df17d57f
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-04-21 16:11:09 +0900

    Update BoardListUI.java
    - 게시글 검색 창의 모양을 좀 더 예쁘게 변경
    - 검색 버튼뿐만 아니라 검색 tf에서도 엔터를 누를 경우 작동하도록 수정

--- code/java/src/gui/BoardListUI.java
+++ code/java/src/gui/BoardListUI.java
@@ -69,17 +69,20 @@ public class BoardListUI implements ActionListener, MouseListener {
 
 	public JPanel createNorthPanel() {
 		JPanel panel = new JPanel();
+		JPanel panel_inner = new JPanel(new BorderLayout());
 		String[] searchTarget = { "제목", "글쓴이" };
 		cb_search = new JComboBox<String>(searchTarget);
 		cb_search.setFont(Commons.getFont());
-		tf_search = new JTextField(10);
+		tf_search = new JTextField(20);
 		tf_search.setFont(Commons.getFont());
+		tf_search.addActionListener(this);
 		btn_search = new JButton("검색");
 		btn_search.setFont(Commons.getFont());
 		btn_search.addActionListener(this);
-		panel.add(cb_search);
-		panel.add(tf_search);
-		panel.add(btn_search);
+		panel_inner.add(cb_search, "West");
+		panel_inner.add(tf_search, "Center");
+		panel_inner.add(btn_search, "East");
+		panel.add(panel_inner);
 		return panel;
 	}
 
@@ -161,11 +164,11 @@ public class BoardListUI implements ActionListener, MouseListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		Object obj = e.getSource();
-		if (obj == btn_search) { // 검색
+		if (obj == btn_search || obj == tf_search) { // 검색
 			String category = cb_search.getSelectedItem().toString();
 			if (category.equals("제목")) {
 				createJtableData(client.searchBoard(MessageVO.BOARD_SEARCH_TITLE, tf_search.getText()));
-			}else if(category.contentEquals("글쓴이")){
+			} else if (category.contentEquals("글쓴이")) {
 				createJtableData(client.searchBoard(MessageVO.BOARD_SEARCH_WRITER, tf_search.getText()));
 			}
 
