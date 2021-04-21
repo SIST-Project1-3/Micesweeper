2ddd4ed40893f8b61057c6914973c3cca2570a4f
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-04-21 12:01:55 +0900

    Update MainUIEvent.java
    - 채팅 메시지 전송시 채팅tf 초기화

--- code/java/src/gui/MainUIEvent.java
+++ code/java/src/gui/MainUIEvent.java
@@ -38,6 +38,7 @@ public class MainUIEvent implements ActionListener, WindowListener, MouseListene
 				msg.setId(ui.client.getId());
 				msg.setContent(ui.tf_chat.getText());
 				ui.client.sendMultiChat(msg);
+				ui.tf_chat.setText("");
 			}
 		}
 	}
