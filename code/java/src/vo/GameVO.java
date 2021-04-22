86a41eff75a24e4bbdbd28a565feca170ddbe1f8
js950830 <82020559+js950830@users.noreply.github.com>
2021-04-22 21:18:18 +0900

    Update GameVO.java
    vo연동

--- code/java/src/vo/GameVO.java
+++ code/java/src/vo/GameVO.java
@@ -6,13 +6,58 @@ import javax.swing.JButton;
 
 // 게임 내 정보들을 담기위한 VO
 public class GameVO {
-	ArrayList<Integer> mice;
-
-	public ArrayList<Integer> getMice() {
+	public ArrayList<JButton> micebtn;
+	public ArrayList<Integer> clickmice;
+	static ArrayList<Integer> mice;
+	static ArrayList<Integer> number;
+	boolean turnflag, winflag, loseflag;
+	int count;
+	public ArrayList<JButton> getMicebtn() {
+		return micebtn;
+	}
+	public void setMicebtn(ArrayList<JButton> micebtn) {
+		this.micebtn = micebtn;
+	}
+	public ArrayList<Integer> getClickmice() {
+		return clickmice;
+	}
+	public void setClickmice(ArrayList<Integer> clickmice) {
+		this.clickmice = clickmice;
+	}
+	public static ArrayList<Integer> getMice() {
 		return mice;
 	}
-
-	public void setMice(ArrayList<Integer> mice) {
-		this.mice = mice;
+	public static void setMice(ArrayList<Integer> mice) {
+		GameVO.mice = mice;
+	}
+	public static ArrayList<Integer> getNumber() {
+		return number;
+	}
+	public static void setNumber(ArrayList<Integer> number) {
+		GameVO.number = number;
+	}
+	public boolean isTurnflag() {
+		return turnflag;
+	}
+	public  void setTurnflag(boolean turnflag) {
+		this.turnflag = turnflag;
+	}
+	public boolean isWinflag() {
+		return winflag;
+	}
+	public void setWinflag(boolean winflag) {
+		this.winflag = winflag;
+	}
+	public boolean isLoseflag() {
+		return loseflag;
+	}
+	public void setLoseflag(boolean loseflag) {
+		this.loseflag = loseflag;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
 	}
 }
