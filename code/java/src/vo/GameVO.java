ae5044d297f61935e20498aa19bf87c39acd1d3b
js950830 <82020559+js950830@users.noreply.github.com>
2021-04-23 10:26:46 +0900

    Update GameVO.java
    버그수정

--- code/java/src/vo/GameVO.java
+++ code/java/src/vo/GameVO.java
@@ -6,10 +6,10 @@ import javax.swing.JButton;
 
 // 게임 내 정보들을 담기위한 VO
 public class GameVO {
-	public ArrayList<JButton> micebtn;
-	public ArrayList<Integer> clickmice;
-	static ArrayList<Integer> mice;
-	static ArrayList<Integer> number;
+	public ArrayList<JButton> micebtn = new ArrayList<JButton>();
+	public ArrayList<Integer> clickmice = new ArrayList<Integer>();
+	public ArrayList<Integer> mice = new ArrayList<Integer>();
+	public ArrayList<Integer> number = new ArrayList<Integer>();
 	boolean turnflag, winflag, loseflag;
 	int count;
 	public ArrayList<JButton> getMicebtn() {
@@ -24,17 +24,17 @@ public class GameVO {
 	public void setClickmice(ArrayList<Integer> clickmice) {
 		this.clickmice = clickmice;
 	}
-	public static ArrayList<Integer> getMice() {
+	public ArrayList<Integer> getMice() {
 		return mice;
 	}
-	public static void setMice(ArrayList<Integer> mice) {
-		GameVO.mice = mice;
+	public void setMice(ArrayList<Integer> mice) {
+		this.mice = mice;
 	}
-	public static ArrayList<Integer> getNumber() {
+	public ArrayList<Integer> getNumber() {
 		return number;
 	}
-	public static void setNumber(ArrayList<Integer> number) {
-		GameVO.number = number;
+	public void setNumber(ArrayList<Integer> number) {
+		this.number = number;
 	}
 	public boolean isTurnflag() {
 		return turnflag;
