5b0deb3e2040b39774fc0eded9abb6ea6b1a95c1
ohdavi <74041787+ohdavi@users.noreply.github.com>
2021-04-25 03:56:15 +0900

    Update MemberVO.java
    프로필 status, setter, getter  삭제

--- code/java/src/vo/MemberVO.java
+++ code/java/src/vo/MemberVO.java
@@ -5,12 +5,8 @@ import java.util.ArrayList;
 
 // DAO를 통해 DB에 접근하기위한 VO
 public class MemberVO implements Serializable{
-	public static int REQUEST_PROFILE = 1; 	// 프로필 정보 요청
-	public static int IMG_REQUEST = 2;	// 프로필 이미지 요청
-	public static int IMG_UPDATE = 3;	// 수정할 이미지 경로 가져오기
 	String id, pw, name, rdate, img; // ID, PW, 이름, 등록일자, 이미지 경로
-	int status, win, lose; // memberVO의 상태, 승리횟수, 패배횟수
-	MemberVO profile;
+	int win, lose; // 승리횟수, 패배횟수
 	
 	public String getId() {
 		return id;
@@ -42,12 +38,6 @@ public class MemberVO implements Serializable{
 	public void setImg(String img) {
 		this.img = img;
 	}
-	public int getStatus() {
-		return status;
-	}
-	public void setStatus(int status) {
-		this.status = status;
-	}
 	public int getWin() {
 		return win;
 	}
@@ -59,12 +49,6 @@ public class MemberVO implements Serializable{
 	}
 	public void setLose(int lose) {
 		this.lose = lose;
-	}
-	public MemberVO getProfile() {
-		return profile;
-	}
-	public void setProfile(MemberVO profile) {
-		this.profile = profile;
 	}
 	
 }
