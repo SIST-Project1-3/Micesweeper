35c22b14d1a92729b22be7d606670ceccf3e4dc0
ohdavi <74041787+ohdavi@users.noreply.github.com>
2021-04-25 03:55:46 +0900

    Update MessageVO.java
    프로필 status, setter, getter 추가

--- code/java/src/vo/MessageVO.java
+++ code/java/src/vo/MessageVO.java
@@ -15,12 +15,16 @@ public class MessageVO implements Serializable {
 	public static int JOIN = 6; // 회원가입
 	public static int IDCHECK = 10; // 아이디 중복확인
 	public static int SERVERCHAT = 7;
+	public static int REQUEST_PROFILE = 11; 	// 프로필 정보 요청
+	public static int IMG_REQUEST = 12;	// 프로필 이미지 요청
+	public static int IMG_UPDATE = 13;	// 수정할 이미지 경로 가져오기
 	int status, no; // MsgVO의 상태, 게시글 No
-	String title, content, id, pw; // 게시글 제목, (내용, 채팅메시지), 작성ID, 패스워드
+	String title, content, id, pw, img; // 게시글 제목, (내용, 채팅메시지), 작성ID, 패스워드, 프로필이미지
 	boolean result; // DAO 결과값
 	ArrayList<BoardVO> boardList; // 게시글 목록 불러오기
 	BoardVO article; // 게시글 읽기
-
+	MemberVO profile; // 프로필 정보
+	
 	public int getStatus() {
 		return status;
 	}
@@ -93,4 +97,21 @@ public class MessageVO implements Serializable {
 		this.article = article;
 	}
 
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public MemberVO getProfile() {
+		return profile;
+	}
+
+	public void setProfile(MemberVO profile) {
+		this.profile = profile;
+	}
+
+	
 }
