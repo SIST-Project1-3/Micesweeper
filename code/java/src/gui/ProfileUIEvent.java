9c83801ded52b46f74986a7e0851f825d9d0e245
ohdavi <74041787+ohdavi@users.noreply.github.com>
2021-04-26 00:43:15 +0900

    Update ProfileUIEvent.java
    프로필 이미지 수정, 선택한 이미지 저장 하는중

--- code/java/src/gui/ProfileUIEvent.java
+++ code/java/src/gui/ProfileUIEvent.java
@@ -8,23 +8,26 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-import vo.BoardVO;
+import vo.MessageVO;
 
 public class ProfileUIEvent implements ActionListener, MouseListener {
 	ProfileUI pui;
 	JFrame f;
 	JButton btn_update, btn_cancel, btn_img1, btn_img2, btn_img3, btn_img4;
 	ImageIcon changeIcon1, changeIcon2, changeIcon3, changeIcon4;
+	String[] img_list;
 //	String[] img_list = { "images/프로필고양이1.jpg", "images/프로필고양이2.jpg", "images/프로필쥐1.jpg", "images/프로필쥐2.png",
 //			"images/고양이.png", "images/쥐.png" };
 //	ArrayList<JButton> btn_list = new ArrayList<JButton>();
@@ -37,22 +40,19 @@ public class ProfileUIEvent implements ActionListener, MouseListener {
 	public void actionPerformed(ActionEvent e) {
 		Object obj = e.getSource();
 
-		if (obj == pui.file_change_btn) { // 프로필 수정
+		if (obj == pui.file_change_btn) { // 프로필 수정 창 생성
+			MessageVO msg = new MessageVO();
+			msg.setStatus(MessageVO.IMG_REQUEST);
+			img_list = pui.client.requestImg(msg);
 			file_form();
-		} else if (obj == btn_update) { // 수정
-//			update_from();
-			if (btn_img1.getMouseListeners() != null) {
-				pui.img_label.setIcon(changeIcon1);
-			}
-//			} else if (btn_img2.getMouseListeners() != null) {
-//				pui.img_label.setIcon(changeIcon2);
-//			} else if (btn_img3.getMouseListeners() != null) {
-//				pui.img_label.setIcon(changeIcon3);
-//			} else if (btn_img4.getMouseListeners() != null) {
-//				pui.img_label.setIcon(changeIcon4);
-			System.out.println("수정");
+		} else if (obj == btn_update) { // 선택한 이미지 저장
+//			System.out.println(btn_img1.getMouseMotionListeners()); //[Ljava.awt.event.MouseMotionListener;@3a1d497c
+//			System.out.println(btn_img1.getIcon()); //javax.swing.ImageIcon@494e706b
+			
+			imgUpdate_from();
+			
 		} else if (obj == btn_cancel) { // 취소
-			// 이미지 다시 선택할 수 있게 만듦
+			// 이미지 다시 선택할 수 있게 만들기
 			btn_img1.setEnabled(true);
 			btn_img2.setEnabled(true);
 			btn_img3.setEnabled(true);
@@ -61,7 +61,6 @@ public class ProfileUIEvent implements ActionListener, MouseListener {
 			btn_img2.setBorder(null);
 			btn_img3.setBorder(null);
 			btn_img4.setBorder(null);
-			System.out.println("취소");
 		} else if (obj == pui.exit_btn) { // 프로필 창 종료
 			pui.f.dispose();
 		}
@@ -70,14 +69,12 @@ public class ProfileUIEvent implements ActionListener, MouseListener {
 	public void file_form() {
 		// 프로필 수정 창 생성
 		f = new JFrame("프로필 수정");
-		JPanel center_panel = new JPanel(new GridLayout(3, 2));
+		JPanel center_panel = new JPanel(new GridLayout(2, 2));
 		JPanel south_panel = new JPanel();
 		JPanel img1_panel = new JPanel();
 		JPanel img2_panel = new JPanel();
 		JPanel img3_panel = new JPanel();
 		JPanel img4_panel = new JPanel();
-		JPanel img5_panel = new JPanel();
-		JPanel img6_panel = new JPanel();
 
 //		for (String str : img_list) {
 //			ImageIcon icon = new ImageIcon("" + str + "");
@@ -91,22 +88,26 @@ public class ProfileUIEvent implements ActionListener, MouseListener {
 //		}
 		
 		// 프로필 이미지 4개
-		ImageIcon icon1 = new ImageIcon("images/프로필고양이1.jpg");
+//		ImageIcon icon1 = new ImageIcon("images/프로필고양이1.jpg");
+		ImageIcon icon1 = new ImageIcon(""+ img_list[0] +"");
 		Image img1 = icon1.getImage();
 		Image changeImg1 = img1.getScaledInstance(200, 200, Image.SCALE_SMOOTH);
 		changeIcon1 = new ImageIcon(changeImg1);
 		btn_img1 = new JButton(changeIcon1);
-		ImageIcon icon2 = new ImageIcon("images/프로필고양이2.jpg");
+//		ImageIcon icon2 = new ImageIcon("images/프로필고양이2.jpg");
+		ImageIcon icon2 = new ImageIcon(""+ img_list[1] +"");
 		Image img2 = icon2.getImage();
 		Image changeImg2 = img2.getScaledInstance(200, 200, Image.SCALE_SMOOTH);
 		changeIcon2 = new ImageIcon(changeImg2);
 		btn_img2 = new JButton(changeIcon2);
-		ImageIcon icon3 = new ImageIcon("images/프로필쥐1.jpg");
+//		ImageIcon icon3 = new ImageIcon("images/프로필쥐1.jpg");
+		ImageIcon icon3 = new ImageIcon(""+ img_list[2] +"");
 		Image img3 = icon3.getImage();
 		Image changeImg3 = img3.getScaledInstance(200, 200, Image.SCALE_SMOOTH);
 		changeIcon3 = new ImageIcon(changeImg3);
 		btn_img3 = new JButton(changeIcon3);
-		ImageIcon icon4 = new ImageIcon("images/프로필쥐2.png");
+//		ImageIcon icon4 = new ImageIcon("images/프로필쥐2.png");
+		ImageIcon icon4 = new ImageIcon(""+ img_list[3] +"");
 		Image img4 = icon4.getImage();
 		Image changeImg4 = img4.getScaledInstance(200, 200, Image.SCALE_SMOOTH);
 		changeIcon4 = new ImageIcon(changeImg4);
@@ -121,7 +122,7 @@ public class ProfileUIEvent implements ActionListener, MouseListener {
 		center_panel.add(img3_panel);
 		center_panel.add(img4_panel);
 		
-		btn_update = new JButton("수정");
+		btn_update = new JButton("확인");
 		btn_cancel = new JButton("취소");
 		btn_update.setFont(Commons.getFont());
 		btn_update.setFont(Commons.getFont());
@@ -145,22 +146,32 @@ public class ProfileUIEvent implements ActionListener, MouseListener {
 			}
 		});
 
-		//f.setResizable(false);
-		f.setSize(500, 750);
+		f.setResizable(false);
+		f.setSize(500, 500);
 		f.setVisible(true);
 	}
 
-	public void update_from() {
-//		if (btn_img1.getMouseListeners() != null) {
-//			btn_img1.setVisible(true);
-//			pui.img_label.setIcon(changeIcon1);
-//		} else if (btn_img2.getMouseListeners() != null) {
-//			pui.img_label.setIcon(changeIcon2);
-//		} else if (btn_img3.getMouseListeners() != null) {
-//			pui.img_label.setIcon(changeIcon3);
-//		} else if (btn_img4.getMouseListeners() != null) {
-//			pui.img_label.setIcon(changeIcon4);
+	public void imgUpdate_from() {
+		// 선택된 이미지 값 넘기기
+		MessageVO msg = new MessageVO();
+		msg.setStatus(MessageVO.IMG_UPDATE);
+		msg.setId(pui.profile.getId());
+		
+//		if() {
+//			msg.setImg(img_list[0]);
+//		}else if() {
+//			msg.setImg(img_list[1]);
+//		}else if() {
+//			msg.setImg(img_list[2]);
+//		}else if() {
+//			msg.setImg(img_list[3]);
 //		}
+		
+		boolean result = pui.client.updateImg(msg);
+		if (result) {
+			JOptionPane.showMessageDialog(null, Commons.getMsg("프로필 이미지가 변경되었습니다."));
+			f.setVisible(false);
+		}
 	}
 
 	@Override
@@ -169,29 +180,36 @@ public class ProfileUIEvent implements ActionListener, MouseListener {
 
 		// 프로필 수정의 이미지중 하나를 눌렀을 때 선택된 이미지 버튼의 바깥쪽에 빨간색 선이 생기게 하고
 		// 선택되지 않은 나머지 이미지 버튼은 비활성화
+		// 선택함과 동시에 내 프로필 창의 픠로필 이미지 변경
 		if (e.getClickCount() == 2) {
 			if (obj == btn_img1) {
 				btn_img1.setBorder(BorderFactory.createLineBorder(Color.RED));
 				btn_img2.setEnabled(false);
 				btn_img3.setEnabled(false);
 				btn_img4.setEnabled(false);
+				pui.img_label.setIcon(changeIcon1);
 			} else if (obj == btn_img2) {
 				btn_img2.setBorder(BorderFactory.createLineBorder(Color.RED));
 				btn_img1.setEnabled(false);
 				btn_img3.setEnabled(false);
 				btn_img4.setEnabled(false);
+				pui.img_label.setIcon(changeIcon2);
 			} else if (obj == btn_img3) {
 				btn_img3.setBorder(BorderFactory.createLineBorder(Color.RED));
 				btn_img1.setEnabled(false);
 				btn_img2.setEnabled(false);
 				btn_img4.setEnabled(false);
+				pui.img_label.setIcon(changeIcon3);
 			} else if (obj == btn_img4) {
 				btn_img4.setBorder(BorderFactory.createLineBorder(Color.RED));
 				btn_img1.setEnabled(false);
 				btn_img2.setEnabled(false);
 				btn_img3.setEnabled(false);
+				pui.img_label.setIcon(changeIcon4);
 			}
 		}
+		
+	
 	}
 
 	@Override
@@ -213,5 +231,5 @@ public class ProfileUIEvent implements ActionListener, MouseListener {
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
 	}
-	
+
 }
