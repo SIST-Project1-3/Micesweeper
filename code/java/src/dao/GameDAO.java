04cb285607e9faeb9374f39e11c53efa15f19f24
ohdavi <74041787+ohdavi@users.noreply.github.com>
2021-04-27 01:50:30 +0900

    Create GameDAO.java
    게임화면에서 프로필보기 눌렀을때 프로필 정보를 가져오기 위한 메소드랑 승패기록 저장을 위한 메소드 만드는중

---
+++ code/java/src/dao/GameDAO.java
@@ -0,0 +1,21 @@
+package dao;
+
+public class GameDAO extends DAO {
+	// Constructor
+	public GameDAO() {
+		System.out.println("4: GameDAO load");
+	}
+
+	// Method
+	// 게임 참가자 프로필 (내 프로필, 상대 프로필)
+	public void getGameProfile() {
+
+	}
+
+	//승패기록 저장
+	public void getScore() {
+
+	}
+
+	
+}
