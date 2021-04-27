603c817e1533b5fd0adffc92da75b5ea82a9ae5d
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-04-27 16:16:05 +0900

    종료 방식 변경

--- code/java/src/gui/StartUIEvent.java
+++ code/java/src/gui/StartUIEvent.java
@@ -20,10 +20,10 @@ public class StartUIEvent implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		Object obj = e.getSource();
-		//로그인 버튼, ID,PW 입력창에서 Enter키를 눌렀을 때 로그인 시도 가능하게 함
+		// 로그인 버튼, ID,PW 입력창에서 Enter키를 눌렀을 때 로그인 시도 가능하게 함
 		if (obj == sui.login_btn || obj == sui.id_tf || obj == sui.pw_tf) {
 			login_check();
-		//회원가입 버튼
+			// 회원가입 버튼
 		} else if (obj == sui.join_btn) {
 			new JoinUI(client);
 		}
@@ -31,7 +31,7 @@ public class StartUIEvent implements ActionListener {
 
 	// 로그인 유효성 검사
 	public void login_check() {
-		//로그인 빈칸 체크
+		// 로그인 빈칸 체크
 		if (sui.id_tf.getText().equals("")) {
 			JOptionPane.showMessageDialog(null, Commons.getMsg("아이디를 입력해주세요"));
 			sui.id_tf.requestFocus();
@@ -45,14 +45,14 @@ public class StartUIEvent implements ActionListener {
 			msg.setId(sui.id_tf.getText());
 			msg.setPw(sui.pw_tf.getText());
 			boolean result = client.login(msg);
-			
+
 			if (result) {
 				JOptionPane.showMessageDialog(null, Commons.getMsg("로그인 되었습니다."));
 				client.setId(sui.id_tf.getText());
 				new MainUI(client);
-				sui.f.setVisible(false);
+				sui.f.dispose();
 			} else {
-				//로그인 실패시 입력했던 정보 리셋
+				// 로그인 실패시 입력했던 정보 리셋
 				JOptionPane.showMessageDialog(null, Commons.getMsg("아이디 또는 패스워드가 틀렸습니다."));
 				sui.id_tf.setText("");
 				sui.pw_tf.setText("");
