7ea57edfcb0454f0e5d3eea82d2ec46b18d6ae4c
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-04-28 22:18:30 +0900

    게시판 테이블에 댓글 추가
    - 게시글 작성 쿼리 수정
    - 댓글 작성 기능 추가
    - 테이블 생성 쿼리에 댓글 속성 추가

--- code/oracle/MyOracle_scott_micesweeper.sql
+++ code/oracle/MyOracle_scott_micesweeper.sql
@@ -5,7 +5,8 @@ CREATE TABLE MEMBER(
     WIN NUMBER(5) DEFAULT 0 NOT NULL, -- 승리횟수는 기본값 0, 반드시 입력
     LOSE NUMBER(5) DEFAULT 0 NOT NULL, -- 패배횟수도 기본값 0, 이하동문
     RDATE DATE DEFAULT SYSDATE NOT NULL, -- 등록일자는 기본값 SYSDATE, 반드시 입력
-    IMG VARCHAR2(40) DEFAULT 'images/쥐.png' NOT NULL -- 해당 프로필의 이미지 사진
+    IMG VARCHAR2(40) DEFAULT 'images/쥐.png' NOT NULL, -- 해당 프로필의 이미지 사진
+    COMMENTS VARCHAR2(4000) -- 댓글을 VARCHAR2 형태로 저장함. 댓글 하나는 \n로 구분하여 저장. id와 내용은 : 를 이용하여 구분
 );
 
 -- 게시글 번호 시퀀스 생성
@@ -31,7 +32,6 @@ CREATE TABLE BOARD(
 CREATE TABLE IMG(
   IMG VARCHAR2(40) NOT NULL  
 );
-
 -- 이미지 경로 입력
 INSERT INTO IMG VALUES('images/프로필고양이1.jpg');
 INSERT INTO IMG VALUES('images/프로필고양이2.jpg');
@@ -40,15 +40,16 @@ INSERT INTO IMG VALUES('images/프로필쥐2.png');
 INSERT INTO IMG VALUES('images/고양이.png');
 INSERT INTO IMG VALUES('images/쥐.png');
 
-
 -- 계정 생성
 INSERT INTO MEMBER VALUES('test', '1234', 0, 0, SYSDATE);
 -- 계정 목록 확인
 SELECT * FROM MEMBER;
 -- 게시글 작성
-INSERT INTO BOARD VALUES(BOARD_NO_SEQ.NEXTVAL, 'DB입력 테스트', '내용', 'test', 0, SYSDATE);
+INSERT INTO BOARD(NO, TITLE, CONTENT, ID) VALUES(BOARD_NO_SEQ.NEXTVAL, 'DB입력 테스트', '내용', 'test');
 -- 게시글 목록
 SELECT * FROM BOARD ORDER BY NO DESC;
+-- 게시글 읽기 전 조회수 증가
+UPDATE BOARD SET VIEWCOUNT = VIEWCOUNT + 1 WHERE NO = 42;
 -- 게시글 읽기
 SELECT * FROM BOARD WHERE NO = 1;
 -- 게시글 검색
@@ -57,11 +58,14 @@ SELECT * FROM BOARD WHERE TITLE LIKE '%성%' ORDER BY NO DESC;
 UPDATE BOARD SET TITLE = '수정', CONTENT = '수정됨 ㅎ' WHERE NO = 1;
 -- 게시글 삭제
 DELETE FROM BOARD WHERE NO = 25;
+-- 댓글 작성
+UPDATE BOARD SET COMMENTS = COMMENTS || 'test2:댓글입니다\n' WHERE NO = 48;
+
 -- 이미지 목록
 SELECT * FROM IMG;
 -- 프로필 이미지 업데이트
-update member set img=? where id=?
 UPDATE MEMBER SET IMG='images/고양이' WHERE ID='test';
+
 -- 승리 횟수 증가 
 UPDATE MEMBER SET WIN = WIN+1 WHERE ID='test';
 -- 패배 횟수 증가
