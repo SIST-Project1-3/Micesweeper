71b2b3e057955ca2f3944dcfc69617faae5618a7
ohdavi <74041787+ohdavi@users.noreply.github.com>
2021-04-28 15:37:54 +0900

    Update GameUIEvent.java
    바뀐 변수명 업데이트

--- code/java/src/gui/GameUIEvent.java
+++ code/java/src/gui/GameUIEvent.java
@@ -39,37 +39,38 @@ public class GameUIEvent implements ActionListener, WindowListener, MouseListene
 		clickmice = gvo.getClickmice();
 		Object obj = e.getSource();
 
-		if (obj == ui.watchprofilebtn) {// 방장 프로필
+		if (obj == ui.watch_profile_btn) { // 방장 프로필 보기
 			MessageVO msg = new MessageVO();
 			msg.setStatus(MessageVO.GAME_PROFILE);
-			msg.setId(ui.bangjangidlabel.getText());
+			msg.setId(ui.master_id_label.getText());
 			MemberVO gameProfile = gdao.getGameProfileResult(msg);
 			ProfileUI pui = new ProfileUI();
 			pui.game_Profile(gameProfile);
-		} else if (obj == ui.send || obj == ui.textField) { // 채팅
-			ui.client.sendGameChat(ui.textField.getText());
-			ui.textField.setText("");
+		} else if (obj == ui.send_btn || obj == ui.chat_tf) { // 채팅
+			ui.client.sendGameChat(ui.chat_tf.getText());
+			ui.chat_tf.setText("");
 			System.out.println("텍스트");
-		} else if (obj == ui.readybutton) { // 레디
-			gsc.turnflag=true;//임시로 해놓은것
-		} else if (obj == ui.exitbutton) { // 나가기
-			if(gvo.isGameflag()==true) {
+		} else if (obj == ui.ready_btn) { // 준비
+			gsc.turnflag = true;  // 임시로 해놓은것
+		} else if (obj == ui.exit_btn) { // 나가기
+			if (gvo.isGameflag() == true) {
 				int answer = JOptionPane.showConfirmDialog(null, Commons.getMsg("게임 중 종료시 패배처리됩니다. 정말로 종료하시겠습니까?"));
 				if (answer == 0) {
 					gvo.setLoseflag(true);
 					exit();
-				//게임종료시 패배처리됩니다 종료하시겠습니까
-				
+					// 게임종료시 패배처리됩니다 종료하시겠습니까
+				}
+			} else {
+				exit();
 			}
-			}else {exit();}
-		} else if (obj == ui.watchprofilebtn_2) { // 도전자 프로필
+		} else if (obj == ui.watch_profile_btn2) { // 참가자 프로필 보기
 			MessageVO msg = new MessageVO();
 			msg.setStatus(MessageVO.GAME_PROFILE);
-			msg.setId(ui.idlabel.getText());
+			msg.setId(ui.user_id_label.getText());
 			MemberVO gameProfile = gdao.getGameProfileResult(msg);
 			ProfileUI pui = new ProfileUI();
 			pui.game_Profile(gameProfile);
-		} else if(gsc.turnflag == true){ // 지뢰 버튼 클릭 처리
+		} else if (gsc.turnflag == true) { // 지뢰 버튼 클릭 처리
 
 //			String btnName = ((JButton) obj).getName();
 			int btnName = Integer.parseInt(((JButton) obj).getName());
@@ -77,7 +78,7 @@ public class GameUIEvent implements ActionListener, WindowListener, MouseListene
 			clickmice.add(btnNo); // 지뢰 클릭 시 클릭된 지뢰 리스트에 추가
 			System.out.println(clickmice);
 			gvo.setClickmice(clickmice); // 리스트 갱신
-			gvo.setClickflag(true);//클릭했다는 정보 갱신
+			gvo.setClickflag(true);// 클릭했다는 정보 갱신
 			ui.gsc.calcBtnClick();
 			System.out.println(gvo.getClickmice());// 이따삭제
 		}
@@ -92,14 +93,14 @@ public class GameUIEvent implements ActionListener, WindowListener, MouseListene
 	public void win() {
 		MessageVO msg = new MessageVO();
 		msg.setStatus(MessageVO.WIN_OR_LOSE);
-		msg.setId(ui.bangjangidlabel.getText());
+		msg.setId(ui.watch_profile_btn.getText());
 		gdao.getWinResult(msg);
 	}
 
 	public void lose() {
 		MessageVO msg = new MessageVO();
 		msg.setStatus(MessageVO.WIN_OR_LOSE);
-		msg.setId(ui.bangjangidlabel.getText());
+		msg.setId(ui.watch_profile_btn.getText());
 		gdao.getLoseResult(msg);
 	}
 
@@ -111,15 +112,17 @@ public class GameUIEvent implements ActionListener, WindowListener, MouseListene
 
 	@Override
 	public void windowClosing(WindowEvent e) { // 나가기
-		if(gvo.isGameflag()==true) {
+		if (gvo.isGameflag() == true) {
 			int answer = JOptionPane.showConfirmDialog(null, Commons.getMsg("게임 중 종료시 패배처리됩니다. 정말로 종료하시겠습니까?"));
 			if (answer == 0) {
 				gvo.setLoseflag(true);
 				exit();
-			//게임종료시 패배처리됩니다 종료하시겠습니까
-			
+				// 게임종료시 패배처리됩니다 종료하시겠습니까
+
+			}
+		} else {
+			exit();
 		}
-		}else {exit();}
 	}
 
 	@Override
